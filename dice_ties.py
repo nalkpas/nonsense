2685882bc1d25fcb4aa600d88f26ae4fb4a4de14
nalkpas <lhefriel@gmail.com>
2018-06-18 22:27:05 -0700

    stupid assymetric intervals

--- dice_ties.py
+++ dice_ties.py
@@ -1,16 +1,16 @@
 import numpy as np
 import sys
 
-num_episodes = 5
-num_trials = int(2.5 * 10**6)
+num_simulations = 5
+num_trials = int(8 * 10**6)
 num_dice = int(sys.argv[1])
 
-for _ in range(num_episodes):
+for _ in range(num_simulations):
 	roll_A = 0
 	roll_B = 0
 	for _ in range(num_dice):
-		roll_A += np.random.randint(1, 6, size = num_trials)
-		roll_B += np.random.randint(1, 6, size = num_trials)
+		roll_A += np.random.randint(1, 7, size = num_trials)
+		roll_B += np.random.randint(1, 7, size = num_trials)
 
 	ties = roll_A == roll_B
 	print(np.sum(ties) / num_trials)
